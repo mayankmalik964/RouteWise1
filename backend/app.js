45fee34308a1f5536366cf5fbe24e9134ac35558
12345rams <vijay90054442298@gmail.com>
2025-02-10 01:58:00 +0530

    adding server.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(moragn('dev'));
 app.use(express.json());
 //routes
+app.use('/api',(req,res,next)=>{
+    res.send('Hello from the API');
+});
 app.use('*', (req, res, next) => {
     console.log('Request was made to: ' + req.originalUrl);
     return next();
